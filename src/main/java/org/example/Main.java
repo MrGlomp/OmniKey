7ea1df28105b49358fc55cdcfd535a5e6a944c68
MrGlomp <e.concha04@ufromail.cl>
2026-06-14 20:41:12 -0400

    Inicio Proyecto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hola");
         System.out.println("CHAO");
         System.out.println("algo");
-        System.out.println("Deja el valo nico");
+        System.out.println("Deja el valo nlol");
     }
 }
